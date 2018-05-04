b16a06aa45f5d58b4f2c975e4138c632506e0591
ivanzy <idimitryz@gmail.com>
2018-05-04 14:03:06 -0300

    sending messages to reasoner

--- processor/process-message.js
+++ processor/process-message.js
@@ -6,7 +6,6 @@ module.exports.process = message => {
   //convert JSON string to JavaScript Object
   let msg = JSON.parse(message.toString());
   let checkedMessage = checkAnomaly(message);
- 
 };
 
 var checkAnomaly = message => {
@@ -25,8 +24,16 @@ var checkAnomaly = message => {
               )
             );
           return { valid: false };
+        } else {
+          axios
+            .post(`http://${nconf.get("REASONER_ADDRESS")}/message`, msg)
+            .then(response => console.log("message sent to reasoner"))
+            .catch(error =>
+              console.log(
+                "could not connect to reasoner server to send NEW MESSAGE"
+              )
+            );
         }
   }
   return { valid: true, message: msg };
 };
-
